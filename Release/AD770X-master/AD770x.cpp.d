e3036ca7fc596f843088e9e21dab63749f460b1d
Unknown <ziplock9000@msn.com>
2018-10-01 16:40:25 +0100

    First code (untested)

---
+++ Release/AD770X-master/AD770x.cpp.d
@@ -0,0 +1,14 @@
+C:\Users\ziplo\AppData\Local\Temp\VMBuilds\VIPDAQ\uno\Release\AD770X-master\AD770x.cpp.o: \
+ C:\Repos\Arduino\libraries\AD770X-master\AD770x.cpp \
+ C:\Repos\Arduino\libraries\AD770X-master\AD770X.h \
+ C:\Program\ Files\ (x86)\Arduino\hardware\arduino\avr\cores\arduino/Arduino.h \
+ C:\Program\ Files\ (x86)\Arduino\hardware\arduino\avr\cores\arduino/binary.h \
+ C:\Program\ Files\ (x86)\Arduino\hardware\arduino\avr\cores\arduino/WCharacter.h \
+ C:\Program\ Files\ (x86)\Arduino\hardware\arduino\avr\cores\arduino/WString.h \
+ C:\Program\ Files\ (x86)\Arduino\hardware\arduino\avr\cores\arduino/HardwareSerial.h \
+ C:\Program\ Files\ (x86)\Arduino\hardware\arduino\avr\cores\arduino/Stream.h \
+ C:\Program\ Files\ (x86)\Arduino\hardware\arduino\avr\cores\arduino/Print.h \
+ C:\Program\ Files\ (x86)\Arduino\hardware\arduino\avr\cores\arduino/Printable.h \
+ C:\Program\ Files\ (x86)\Arduino\hardware\arduino\avr\cores\arduino/USBAPI.h \
+ C:\Program\ Files\ (x86)\Arduino\hardware\arduino\avr\cores\arduino/Arduino.h \
+ C:\Program\ Files\ (x86)\Arduino\hardware\arduino\avr\variants\standard/pins_arduino.h
--- VIPDAQ.ino
+++ VIPDAQ.ino
@@ -1,9 +1,31 @@
-void setup() {
-  // put your setup code here, to run once:
 
+#include <AD770X.h>
+
+// Init the ADC. The value in parenthesis is the scaler
+AD770X ad7705(65536); //AD770X ad7706(2.5);
+
+void setup()
+{
+	ad7705.reset();
+	ad7705.init(AD770X::CHN_AIN1);	//Ch1
+	ad7705.init(AD770X::CHN_AIN2);	//Ch2
+	Serial.begin(9600);
 }
 
-void loop() {
-  // put your main code here, to run repeatedly:
 
+void loop()
+{
+	unsigned int ADCValue1, ADCValue2;
+	byte b1, b2;
+
+	ADCValue1 = ad7705.readADResult(AD770X::CHN_AIN1);
+	ADCValue2 = ad7705.readADResult(AD770X::CHN_AIN2);
+	Serial.print(ADCValue1);
+	Serial.print(":");
+	Serial.println(ADCValue2);
+
+	// Convert data to 2 bytes for sending
+	//b1 = ADCValue & 0xFF;
+	//b2 = (ADCValue >> 8) & 0xFF;
+	delay(1000);
 }
