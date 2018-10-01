a7640172b8d72cd3b50e4984d630214e301ec557
Unknown <ziplock9000@msn.com>
2018-10-01 21:56:48 +0100

    More refinement of channel 1

--- VIPDAQ.ino
+++ VIPDAQ.ino
@@ -1,5 +1,10 @@
 
-#define LoopSize 500UL //500 = 1 per second. 
+const unsigned long ONE_SECOND = 500UL; //500 = 1 per second. 
+const unsigned long FIVE_SECONDS = (ONE_SECOND * 5);
+const unsigned long ONE_MINUTE = (ONE_SECOND * 60);
+const unsigned long FIVE_MINUTES = (ONE_MINUTE * 5);
+const unsigned long LoopSize = FIVE_SECONDS;
+
 #include <AD770X.h>
 
 // Init the ADC. The value in parenthesis is the scaler
@@ -11,6 +16,7 @@ void setup()
 	ad7705.reset();
 	ad7705.init(AD770X::CHN_AIN1);
 	//ad7705.init(AD770X::CHN_AIN2);
+
 }
 
 
@@ -19,11 +25,14 @@ void loop()
 	
 	unsigned long accumulator;
 	accumulator = 0;
+
 	for(int i=0;i<LoopSize;i++)
 	{
 		accumulator += ad7705.readADResult(AD770X::CHN_AIN1);
 	}
-	Serial.print((accumulator / LoopSize)*0.000076295109);
+	float a = accumulator / LoopSize;
+	float b = (a*0.000076295109)-0.002289;
+	Serial.print(b,6);
 	Serial.println('v');
 	
 
