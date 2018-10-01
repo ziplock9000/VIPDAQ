9037e5d3b3dd766ad40c11c1e8c70503885d2d80
Unknown <ziplock9000@msn.com>
2018-10-01 21:17:16 +0100

    One channel sorta working

--- VIPDAQ.ino
+++ VIPDAQ.ino
@@ -1,31 +1,38 @@
 
+#define LoopSize 500UL //500 = 1 per second. 
 #include <AD770X.h>
 
 // Init the ADC. The value in parenthesis is the scaler
-AD770X ad7705(65536); //AD770X ad7706(2.5);
+AD770X ad7705(2.5); //AD770X ad7706(2.5);
 
 void setup()
 {
-	ad7705.reset();
-	ad7705.init(AD770X::CHN_AIN1);	//Ch1
-	ad7705.init(AD770X::CHN_AIN2);	//Ch2
 	Serial.begin(9600);
+	ad7705.reset();
+	ad7705.init(AD770X::CHN_AIN1);
+	//ad7705.init(AD770X::CHN_AIN2);
 }
 
 
 void loop()
 {
-	unsigned int ADCValue1, ADCValue2;
-	byte b1, b2;
+	
+	unsigned long accumulator;
+	accumulator = 0;
+	for(int i=0;i<LoopSize;i++)
+	{
+		accumulator += ad7705.readADResult(AD770X::CHN_AIN1);
+	}
+	Serial.print((accumulator / LoopSize)*0.000076295109);
+	Serial.println('v');
+	
 
-	ADCValue1 = ad7705.readADResult(AD770X::CHN_AIN1);
-	ADCValue2 = ad7705.readADResult(AD770X::CHN_AIN2);
+	/*
+	unsigned int ADCValue1 = ad7705.readADResult(AD770X::CHN_AIN1);
+	unsigned int ADCValue2 = ad7705.readADResult(AD770X::CHN_AIN2);
 	Serial.print(ADCValue1);
 	Serial.print(":");
 	Serial.println(ADCValue2);
-
-	// Convert data to 2 bytes for sending
-	//b1 = ADCValue & 0xFF;
-	//b2 = (ADCValue >> 8) & 0xFF;
 	delay(1000);
+	*/
 }
