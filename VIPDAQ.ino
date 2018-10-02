8216b31dd74dceffe07dec12c0f699bd97bcd072
Unknown <ziplock9000@msn.com>
2018-10-02 11:32:29 +0100

    Tidied and optimised code

--- VIPDAQ.ino
+++ VIPDAQ.ino
@@ -1,14 +1,16 @@
 
 const unsigned long ONE_SECOND = 500UL; //500 = 1 per second. 
-const unsigned long FIVE_SECONDS = (ONE_SECOND * 5);
-const unsigned long ONE_MINUTE = (ONE_SECOND * 60);
+const unsigned long FIVE_SECONDS = (ONE_SECOND * 5); //168.39 .46 .59
+const unsigned long ONE_MINUTE = (ONE_SECOND * 60); //168.77
 const unsigned long FIVE_MINUTES = (ONE_MINUTE * 5);
-const unsigned long LoopSize = FIVE_SECONDS;
+const unsigned long LoopSize = FIVE_MINUTES;
 
 #include <AD770X.h>
 
 // Init the ADC. The value in parenthesis is the scaler
 AD770X ad7705(2.5); //AD770X ad7706(2.5);
+
+unsigned long zerotime;
 
 void setup()
 {
@@ -17,6 +19,12 @@ void setup()
 	ad7705.init(AD770X::CHN_AIN1);
 	//ad7705.init(AD770X::CHN_AIN2);
 
+	Serial.print ("Sample window :");
+	Serial.print(LoopSize);
+	Serial.println("ms");
+	Serial.println("H:M:S - mA");
+
+	zerotime = millis();
 }
 
 
@@ -26,15 +34,17 @@ void loop()
 	unsigned long accumulator;
 	accumulator = 0;
 
-	for(int i=0;i<LoopSize;i++)
+	for(unsigned long i=0;i<LoopSize;i++)
 	{
 		accumulator += ad7705.readADResult(AD770X::CHN_AIN1);
 	}
 	float a = accumulator / LoopSize;
-	float b = (a*0.000076295109)-0.002289;
-	Serial.print(b,6);
-	Serial.println('v');
-	
+	float millivolts = ((a*0.000076295109)-0.00076)*1000;
+
+	Serial.print(DurationInHMSstring());
+	Serial.print(" - ");
+	Serial.print(millivolts, 2);
+	Serial.println("mA");
 
 	/*
 	unsigned int ADCValue1 = ad7705.readADResult(AD770X::CHN_AIN1);
@@ -45,3 +55,28 @@ void loop()
 	delay(1000);
 	*/
 }
+
+char * DurationInHMSstring()
+{
+	return MilliSecondsToHMSstring(millis() - zerotime);
+}
+
+char * MilliSecondsToHMSstring(unsigned long time_in_ms)
+{
+	return SecondsToHMSstring(time_in_ms / 1000);
+}
+
+char * SecondsToHMSstring(unsigned long time_in_seconds)
+{
+	static char str[12];
+
+	unsigned long h = time_in_seconds / 3600;
+	unsigned long d = h / 24;
+	time_in_seconds = time_in_seconds % 3600;
+	int m = time_in_seconds / 60;
+	int s = time_in_seconds % 60;
+	sprintf(str, "%02ld:%02d:%02d", h, m, s);
+	return str;
+}
+
+
